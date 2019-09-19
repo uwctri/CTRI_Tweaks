b8999aea16a2ed5bcadc6988d775f56527515154
Adam Nunez <adam.a.nunez@gmail.com>
2019-09-19 20:23:02 +0000

    Small PHP cleanup, no functionality changes.

--- CTRItweaks.php
+++ CTRItweaks.php
@@ -13,20 +13,13 @@ class CTRItweaks extends AbstractExternalModule {
     public function redcap_every_page_top($project_id) {
 		
 		if ( $this->getProjectSetting('hide-repeat-vars') ) {
-		
-			// Check to see if we are on the right page
-			if (PAGE != 'DataExport/index.php' || $project_id == NULL) {
+			// Check to see if we are on the right page, and that its not the edit report page
+			if (PAGE != 'DataExport/index.php' || $project_id == NULL || $_GET['addedit']) 
 				return;
-			}
-			// Make sure its not an edit report page
-			if ($_GET['addedit']) {
-				return;
-			}
 			// Check if the report ID is listed in the settings
 			$id_list = array_map('trim',explode(',',$this->getProjectSetting('report-id-list')));
-			if (!in_array($_GET['report_id'],$id_list)) {
+			if (!in_array($_GET['report_id'],$id_list))
 				return;
-			}
 			$this->includeJs('js/hide_repeat_variables.js');
 		}
     }
