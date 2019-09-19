56e5d280bafd042d0ae388747b0f3fd754ef6c63
Adam Nunez <adam.a.nunez@gmail.com>
2019-09-19 20:18:54 +0000

    Update hide_repeat_variables.js to most recent version from other project.

--- js/hide_repeat_variables.js
+++ js/hide_repeat_variables.js
@@ -1,22 +1,13 @@
 $(document).ready(function hrv() {
-    var rtbl = document.getElementById('report_table')
     // Call self until the report table is built in the dom
-    if (rtbl == null) {
+    if ($('#report_table').length === 0) 
         window.requestAnimationFrame(hrv);
-    }
     else {
-        var haystack = ["Repeat Instrument","Repeat Instance"];
-        var tcol = [];
-        // Flip through the header to flag what cols we need to blow away
-        for (let [j,col] of rtbl.rows[0].cells.entries()) {
-            if ($.inArray(col.childNodes[0].data, haystack)>-1)
-                tcol.push(j);
-            if (tcol.length == 2) break;
-        }
-        // Flip through the table and delete the two cols
-        for (let row of rtbl.rows) {
-            row.cells[tcol[0]].remove()
-            row.cells[tcol[1]-1].remove()
-        }
+        const haystack = ["redcap_repeat_instrument","redcap_repeat_instance","redcap_event_name"];
+        $('#report_table th :first-child').not("wbr").each( function(i) {
+            if ( haystack.includes($(this).text()) ) {
+                $(this).parent().hide();
+                $("#report_table td:nth-child("+(i+1)+")").hide();}
+        });
     }
 });
