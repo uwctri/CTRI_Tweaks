cf3f31c58259a22549bc9d878358cdc1e28a2c8e
Adam Nunez <adam.a.nunez@gmail.com>
2020-05-26 13:30:54 -0500

    added on change to fuzzy

--- js/data_entry_action_tag_fuzzy.js
+++ js/data_entry_action_tag_fuzzy.js
@@ -4,12 +4,10 @@ $(document).ready(function () {
         <td style="min-width:40px;padding-right:10px">Record</td>
         <td>Value</td>
     </tr>`
+    let url = new URLSearchParams(location.search);
     $.each( CTRItweaks.fuzzy.search, function(field, data) {
-        const recordID = location.search.split('id=')[1].split('&')[0];
-        const fuse = new Fuse(data, {
-          keys: ['value']
-        })
-        $(`input[name=${field}]`).on('keyup', function() {
+        const fuse = new Fuse(data, {keys: ['value']});
+        $(`input[name=${field}]`).on('keyup change', function() {
             let search = fuse.search($(this).val());
             let display;
             if (search.length == 0) {
@@ -17,12 +15,11 @@ $(document).ready(function () {
             } else {
                 display = search.map( function(value) {
                     value = value.item;
-                    if ( recordID == value.record )
+                    if ( url.get('id') == value.record && url.get('event_id') == value.event && url.get('instance') == value.instance )
                         return '';
                     return CTRItweaks.fuzzy.html.replace('Value', `"${value.value}"`)
                     .replace('Record', `<a href="${location.pathname}?pid=${pid}&page=${value.instrument}&id=${value.record}&event_id=${value.event}&instance=${value.instance}">${value.record}</a>`);
                 }).join('');
-                console.log( search );
                 display = `<table>${display}</table>`;
             }
             if ( $(this).data("bs.popover") )
