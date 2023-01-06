dd11d3442d627c9b6b93f21d4725a3a7c645f5bd
adam nunez <adam.a.nunez@gmail.com>
2023-01-06 02:45:19 -0600

    Cleanup config.js ~

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(() => {
     console.log("Loaded CTRI tweaks config")
-    var $modal = $('#external-modules-configure-modal');
+    let $modal = $('#external-modules-configure-modal');
     $modal.on('show.bs.modal', function () {
         // Making sure we are overriding this modules's modal only.
         if ($(this).data('module') !== CTRItweaks.modulePrefix) return;
@@ -20,68 +20,6 @@ $(document).ready(function () {
             $("tr:contains(tableStart)").remove();
             $("tr:contains(tableEnd)").css('height', (45 * rowCount) + 'px');
             $("tr:contains(tableEnd)").html('');
-            $("tr[field^=write-back-] span").remove();
-            $.each(['hide-form-row', 'hide-events', 'hide-repeating-table', 'full-size-repeating-table'], function () {
-                $(`tr[field=${this}] span`).each(function () {
-                    $(this).text($(this).text().split('.')[1] + '. ');
-                });
-            });
-            $("tr[field^=check-print] span").remove();
-            $("tr[field=write-back-report] span").remove();
-            $.each(['full-size-repeating-table-list', 'hide-repeating-table-list',
-                'hide-events-list', 'hide-form-row-list'], function () {
-                    $(`tr[field=${this}] span`).first().remove();
-                });
-            $("tr[field=write-back-variable-list] .external-modules-add-remove-column").css('padding', '4px 12px 4px 12px')
-
-            // Organze the Write back event/var area
-            $("tr[field=write-back-variable] label").css('transform', 'translateY(-20px)');
-            $("tr[field=write-back-event] td").css('padding-top', '1.5rem').css('padding-bottom', '.25rem');
-            $("tr[field=write-back-variable] td").css('border', 'none').css('padding-top', '.25rem').css('padding-bottom', '.25rem');
-            $("tr[field=write-back-variable] select").css('margin-bottom', '20px');
-
-            // Setup system global drop down
-            $("input[name^=write-back-global]").hide();
-            var ddm = '<select class="external-modules-input-element global-wb-dropdown"><option value=""></option><option>';
-            ddm += Object.keys(CTRItweaks.systemSettings).join("</option><option>") + "</option></select>"
-            $("input[name^=write-back-global]").after(ddm);
-            $(".global-wb-dropdown").on("click", function () {
-                $(this).prev('input').val($(this).val());
-            });
-            $(".global-wb-dropdown").each(function () {
-                $(this).val($(this).prev('input').val());
-            });
-
-            // Hide values based on radio setting
-            $("input[name^=write-back-to_]").on("click", function () {
-                var [, a, b] = $(this).prop('name').split('____');
-                switch ($(this).val()) {
-                    case 'var':
-                        $(`select[name=write-back-variable____${a}____${b}]`).closest('tr').show();
-                        $(`input[name=write-back-global____${a}____${b}]`).closest('tr').hide();
-                        break;
-                    case 'global':
-                        $(`select[name=write-back-variable____${a}____${b}]`).closest('tr').hide();
-                        $(`input[name=write-back-global____${a}____${b}]`).closest('tr').show();
-                        break;
-                    case 'both':
-                        $(`select[name=write-back-variable____${a}____${b}]`).closest('tr').show();
-                        $(`input[name=write-back-global____${a}____${b}]`).closest('tr').show();
-                        break;
-                }
-            });
-
-            // Default the radio value correctly
-            $("input[name^=write-back-to_]").each(function (index) {
-                if (index % 3 != 0) return;
-                if (!$(this).is(":checked") &&
-                    !$(this).siblings("input").first().is(":checked") &&
-                    !$(this).siblings("input").last().is(":checked"))
-                    $(this).click();
-            });
-
-            // Click all active buttons
-            $("input[name^=write-back-to_]:checked").click();
         };
     });
 
